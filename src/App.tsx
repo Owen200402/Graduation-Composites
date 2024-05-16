cb5a7aeae699a587fd82785efcf1529d7b266969
Owen Zheng <zcc2280411284@gmail.com>
2024-05-16 11:47:09 -0700

    project started

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import GraduationFilterAlpha from './components/GraduationFilterAlpha'
+import GraduationFilterYear from './components/GraduationFilterYear'
+
+function App() {
+  return <div>Hello World</div>
+}
+
+
+export default App
