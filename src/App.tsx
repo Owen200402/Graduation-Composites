77f68b8c3cfb5112719dca08694485aede5d28fd
Owen Zheng <zcc2280411284@gmail.com>
2024-05-16 13:19:40 -0700

    Created basic filter by year component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import GraduationFilterAlpha from './components/GraduationFilterAlpha'
 import GraduationFilterYear from './components/GraduationFilterYear'
 
 function App() {
-  return <div>Hello World</div>
+  return <>
+  <GraduationFilterYear></GraduationFilterYear>
+  </>
 }
 
 
--- src/components/GraduationFilterYear.tsx
+++ src/components/GraduationFilterYear.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const GraduationFilterYear = () => {
   return (
-    <div>
-      
-    </div>
+    <select className="form-select" onChange={(event) => console.log(event.target.value)}>
+      <option value=""></option>
+      <option value="2013">2012</option>
+      <option value="2013">2013</option>
+      <option value="2014">2014</option>
+      <option value="2015">2015</option>
+    </select>
   )
 }
 
