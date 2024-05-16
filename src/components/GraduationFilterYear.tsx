08baa45d9ce4e48be75f707b5a4c9116d9657df3
Owen Zheng <zcc2280411284@gmail.com>
2024-05-16 15:59:17 -0700

    Fixed file name from 1969->1970

--- src/components/GraduationFilterYear.tsx
+++ src/components/GraduationFilterYear.tsx
@@ -13,7 +13,7 @@ const GraduationFilterYear = () => {
         }}
       >
         <option value=""></option>
-        <option value="1969">1969</option>
+        <option value="1970">1970</option>
         <option value="2023">2023</option>
       </select>
       <h2>Graduation Class of {year}</h2>
