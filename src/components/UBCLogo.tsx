9aba12c0ce93c5707765a07e94e85ddb41b53eac
Owen Zheng <zcc2280411284@gmail.com>
2024-05-22 09:27:12 -0700

    Added the logo for ECE to the banner.

--- src/components/UBCLogo.tsx
+++ src/components/UBCLogo.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const Image = styled.img`
+  height: 6rem;
+  object-fit: cover;
+  margin: 0.8rem;
+`;
+
+const Div = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const UBCLogo = () => {
   return (
-    <div>
-      <img src="assets/ubc-logo-2018-fullsig-blue-rgb72.jpg" alt="UBC Logo" />
-    </div>
+    <Div>
+      <Image src="assets/ubc-logo-2018-fullsig-blue-rgb72.jpg" alt="UBC Logo" />
+      <Image src="assets/ECE-Logo.png" alt="ECE Logo" />
+    </Div>
   );
 };
 
