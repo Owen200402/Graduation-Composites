a9b388038ec0f189c9555b8c7524b9d849c8cbdf
Owen Zheng <zcc2280411284@gmail.com>
2024-05-24 15:02:40 -0700

    Added ts command for sending D-iD api requests.

---
+++ src/getTalkScript.ts
@@ -0,0 +1,12 @@
+const fetchOptions: RequestInit = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      authorization: 'Basic emNjMjI4MDQxMTI4NEBnbWFpbC5jb20:Crsb7EWnoyLUMRQcbLoX5'
+    },
+  };
+  
+fetch('https://api.d-id.com/talks', fetchOptions)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
