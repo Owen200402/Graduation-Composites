3fbab26c0cbd6091dbfa4f53c373d8ff11c95dd2
Owen Zheng <zcc2280411284@gmail.com>
2024-05-28 12:58:25 -0700

    .

--- src/components/BottomBanner.tsx
+++ src/components/BottomBanner.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 const Banner = styled.div`
-  background-image: url('/images/bottom-banner.webp');
+  background-image: url('images/bottom-banner.webp');
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center top;
