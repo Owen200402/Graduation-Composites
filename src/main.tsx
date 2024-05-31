d04613d2d604564c709b940ba49f6efb46d94de6
Owen Zheng <zcc2280411284@gmail.com>
2024-05-31 09:38:49 -0700

    .

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "animate.css";
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import GuardianEgypt-Black-Regular from "./font/guardian_egyptian_headline"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
