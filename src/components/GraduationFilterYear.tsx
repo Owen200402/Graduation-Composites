1cbb2a8038c6e8a786e4474f201544506f95bfb2
Owen Zheng <zcc2280411284@gmail.com>
2024-06-06 13:28:28 -0700

    Change dropup to dropdown with two columns.

--- src/components/GraduationFilterYear.tsx
+++ src/components/GraduationFilterYear.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 interface Props {
   onSelect: (year: number) => void;
 }
@@ -9,7 +10,7 @@ const GraduationFilterYear = ({ onSelect }: Props) => {
   const [year, setYear] = useState(1930);
   return (
     <>
-      <div className="dropup m-2">
+      <div className="m-2">
         <button
           className="btn btn-secondary dropdown-toggle"
           type="button"
@@ -18,80 +19,68 @@ const GraduationFilterYear = ({ onSelect }: Props) => {
         >
           Year of Graduation
         </button>
-        <ul className="dropdown-menu">
-          <li>
-            <button
-              className="dropdown-item"
-              type="button"
-              onClick={() => {
-                onSelect(1930);
-                setYear(1930);
-              }}
-            >
-              1930
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item"
-              type="button"
-              onClick={() => {
-                onSelect(1937);
-                setYear(1937);
-              }}
-            >
-              1937
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item"
-              type="button"
-              onClick={() => {
-                onSelect(1938);
-                setYear(1938);
-              }}
-            >
-              1938
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item"
-              type="button"
-              onClick={() => {
-                onSelect(1939);
-                setYear(1939);
-              }}
-            >
-              1939
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item"
-              type="button"
-              onClick={() => {
-                onSelect(1940);
-                setYear(1940);
-              }}
-            >
-              1940
-            </button>
-          </li>
-          <li>
-            <button
-              className="dropdown-item"
-              type="button"
-              onClick={() => {
-                onSelect(1941);
-                setYear(1941);
-              }}
-            >
-              1941
-            </button>
-          </li>
-        </ul>
+        <div className="dropdown-menu multi-column">
+          <button
+            className="dropdown-item"
+            type="button"
+            onClick={() => {
+              onSelect(1930);
+              setYear(1930);
+            }}
+          >
+            1930
+          </button>
+          <button
+            className="dropdown-item"
+            type="button"
+            onClick={() => {
+              onSelect(1937);
+              setYear(1937);
+            }}
+          >
+            1937
+          </button>
+          <button
+            className="dropdown-item"
+            type="button"
+            onClick={() => {
+              onSelect(1938);
+              setYear(1938);
+            }}
+          >
+            1938
+          </button>
+          <button
+            className="dropdown-item"
+            type="button"
+            onClick={() => {
+              onSelect(1939);
+              setYear(1939);
+            }}
+          >
+            1939
+          </button>
+          <button
+            className="dropdown-item"
+            type="button"
+            onClick={() => {
+              onSelect(1940);
+              setYear(1940);
+            }}
+          >
+            1940
+          </button>
+          <button
+            className="dropdown-item"
+            type="button"
+            onClick={() => {
+              onSelect(1941);
+              setYear(1941);
+            }}
+          >
+            1941
+          </button>
+        </div>
       </div>
     </>
   );
