4700845907eac4648e2d9a4a0fc57dac922ff676
Owen Zheng <zcc2280411284@gmail.com>
2024-06-12 16:51:09 -0700

    Added polyfills.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "animate.css";
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'animate.css';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
   plugins: [
-    react()]
+    react(),
+  ],
+  build: {
+    target: ['chrome39'],
+  },
+  optimizeDeps: {
+    include: ['core-js/stable', 'regenerator-runtime/runtime']
+  }
 });
