92d1e1830e4dfcda6dfc341e9158b813390b7904
Owen Zheng <zcc2280411284@gmail.com>
2024-06-12 14:09:02 -0700

    Removed babel setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,5 @@ import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
   plugins: [
-    react(),
-    babel({
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      presets: [
-        ['@babel/preset-env', {
-          targets: '> 0.25%, not dead, not op_mini all',
-          useBuiltIns: 'entry',
-          corejs: 3,
-        }],
-        '@babel/preset-react'
-      ]
-    })
-  ],
+    react()]
 });
