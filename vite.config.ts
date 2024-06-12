da63db220621bded7d05358bb0bf6dc518d8f05b
Owen Zheng <zcc2280411284@gmail.com>
2024-06-12 16:53:01 -0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     react(),
-  ],
-  build: {
-    target: ['chrome39'],
-  },
-  optimizeDeps: {
-    include: ['core-js/stable', 'regenerator-runtime/runtime']
-  }
+  ]
 });
