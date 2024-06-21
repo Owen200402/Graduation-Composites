7360aabd8d0dac78df834ae5ef8627deb93460a4
Owen Zheng <zcc2280411284@gmail.com>
2024-06-21 15:50:32 -0700

    Added AI button.

--- src/App.tsx
+++ src/App.tsx
@@ -55,6 +55,8 @@ function App() {
 
   const [isAtMainScreen, setAtMainScreen] = useState(true);
 
+  const [AIVisibility, setAIVisibility] = useState(false);
+
   const { outputs, errors } = useThinPlate({ imageUrls: ImageLinkPaths });
 
   const theme = useTheme();
@@ -235,23 +237,36 @@ function App() {
         </div>
 
         <BottomBanner></BottomBanner>
-        <div>
-          <h2 style={{textAlign: "center"}}>Thin Plate Spline Motion Model (under development)</h2>
-          {errors.length >= 1 && <p>Error: {errors}</p>}
-          {outputs && (
-            <div>
-              {outputs.map((output, index) => (
-                <div key={index}>
-                  <h4>Result {index + 1}</h4>
-                  <pre>{JSON.stringify(output, null, 2)}</pre>
-                  {ImageLinkPaths.length === index + 1 && (
-                    <h4 style={{ color: 'green', marginBottom: "3rem" }}>Done!</h4>
-                  )}
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
+        <button
+          onClick={() => setAIVisibility(true)}
+          className="btn btn-primary"
+          style={{ marginBottom: '2rem' }}
+        >
+          Thin Plate AI
+        </button>
+        {AIVisibility && (
+          <div>
+            <h2 style={{ textAlign: 'center' }}>
+              Thin Plate Spline Motion Model (under development)
+            </h2>
+            {errors.length >= 1 && <p>Error: {errors}</p>}
+            {outputs && (
+              <div>
+                {outputs.map((output, index) => (
+                  <div key={index}>
+                    <h4>Result {index + 1}</h4>
+                    <pre>{JSON.stringify(output, null, 2)}</pre>
+                    {ImageLinkPaths.length === index + 1 && (
+                      <h4 style={{ color: 'green', marginBottom: '3rem' }}>
+                        Done!
+                      </h4>
+                    )}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </>
   );
