d02c64e31c0d39814d0116e8f4465e284f87168e
Owen Zheng <zcc2280411284@gmail.com>
2024-06-25 11:57:58 -0700

    .

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import ScreenSearchDesktopIcon from '@mui/icons-material/ScreenSearchDesktop';
 import { photoData } from '../components/photoData';
 import { TextField } from '@mui/material';
