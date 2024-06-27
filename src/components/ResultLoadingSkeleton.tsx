62be156c72400b2726516aa4eb898ca526cb7871
Owen Zheng <zcc2280411284@gmail.com>
2024-06-27 14:49:34 -0700

    Improved the result loading skeleton

--- src/components/ResultLoadingSkeleton.tsx
+++ src/components/ResultLoadingSkeleton.tsx
@@ -3,12 +3,12 @@ import { Skeleton } from '@mui/material';
 const ResultLoadingSkeleton = () => {
   return (
     <div>
-      <Skeleton variant="text" width={100} sx={{ margin: '1rem' }} />
+      <Skeleton variant="text" width={190} />
       <Skeleton
         variant="rectangular"
         width={190}
         height={190}
-        sx={{ margin: '1rem' }}
+        // sx={{ margin: '1rem' }}
       />
       <Skeleton variant="text" width={190} />
     </div>
