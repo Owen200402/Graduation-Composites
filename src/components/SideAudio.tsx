ecfe526aca683b8267eeb2973fd4f1f74d6f2361
Owen Zheng <zcc2280411284@gmail.com>
2024-08-02 15:24:01 -0700

    Add song name and singers with obj json mapping.

--- src/components/SideAudio.tsx
+++ src/components/SideAudio.tsx
@@ -1,14 +1,14 @@
-import AspectRatio from '@mui/joy/AspectRatio';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
-import Typography from '@mui/joy/Typography';
-import { useMediaQuery } from '@mui/material';
-import ReactAudioPlayer from 'react-audio-player';
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
-import { FaMusic } from 'react-icons/fa6';
-import useAudioStore from '../stores/audioStore';
-import useNavigationStore from '../stores/navigationStore';
+import AspectRatio from "@mui/joy/AspectRatio";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import { Typography, useMediaQuery } from "@mui/material";
+import ReactAudioPlayer from "react-audio-player";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import { FaMusic } from "react-icons/fa6";
+import useAudioStore from "../stores/audioStore";
+import useNavigationStore from "../stores/navigationStore";
+import audioData from "../data/audioData";
 
 interface Props {
   onClickSelect: (
@@ -17,37 +17,42 @@ interface Props {
 }
 
 const SideAudio = ({ onClickSelect }: Props) => {
-   const { selectedYear } = useNavigationStore();
-   const { type } = useAudioStore();
-   const isScreenLarge = useMediaQuery('(min-width:1024px)');
-   const decade = selectedYear !== undefined ? Math.floor(selectedYear / 10) * 10 : null;
- 
-   if (selectedYear === undefined) {
-     return <div></div>;
-   }
+  const { selectedYear } = useNavigationStore();
+  const { type } = useAudioStore();
+  const isScreenLarge = useMediaQuery("(min-width:1024px)");
+  const decade =
+    selectedYear !== undefined ? Math.floor(selectedYear / 10) * 10 : null;
+
+  const currentSong = audioData.filter(
+    (audio) => audio.year === decade && audio.type === type
+  );
+
+  if (selectedYear === undefined) {
+    return <div></div>;
+  }
 
   return isScreenLarge ? (
     <div>
       <Card
         sx={{
           width: 190,
-          position: 'absolute',
+          position: "absolute",
           top: "20%",
-          animation: 'fadeInLeft 1s ease-in-out;',
-          backgroundColor: '#FAFAFA',
+          animation: "fadeInLeft 1s ease-in-out;",
+          backgroundColor: "#FAFAFA",
         }}
       >
         <FaMusic
           style={{
-            position: 'absolute',
+            position: "absolute",
             right: 0,
             top: 0,
-            fontSize: '22px',
-            color: '#f5770a',
+            fontSize: "22px",
+            color: "#f5770a",
           }}
         />
         <div>
-          <Typography level="title-lg">Play {decade}'s Music</Typography>
+          <Typography variant="h5">Play Music: {decade}</Typography>
           <div className="btn-group dropend">
             <button
               type="button"
@@ -62,7 +67,7 @@ const SideAudio = ({ onClickSelect }: Props) => {
                 <a
                   className="dropdown-item"
                   onClick={onClickSelect}
-                  style={{ cursor: 'pointer' }}
+                  style={{ cursor: "pointer" }}
                 >
                   Cinematic
                 </a>
@@ -71,7 +76,7 @@ const SideAudio = ({ onClickSelect }: Props) => {
                 <a
                   className="dropdown-item"
                   onClick={onClickSelect}
-                  style={{ cursor: 'pointer' }}
+                  style={{ cursor: "pointer" }}
                 >
                   Canadian
                 </a>
@@ -80,13 +85,17 @@ const SideAudio = ({ onClickSelect }: Props) => {
                 <a
                   className="dropdown-item"
                   onClick={onClickSelect}
-                  style={{ cursor: 'pointer' }}
+                  style={{ cursor: "pointer" }}
                 >
                   Dance
                 </a>
               </li>
               <li>
-                <a className="dropdown-item" onClick={onClickSelect} style={{cursor: "pointer"}}>
+                <a
+                  className="dropdown-item"
+                  onClick={onClickSelect}
+                  style={{ cursor: "pointer" }}
+                >
                   Romantic
                 </a>
               </li>
@@ -97,14 +106,22 @@ const SideAudio = ({ onClickSelect }: Props) => {
           <img src={`assets/displayOnAudioPlayer/${decade}.webp`} />
         </AspectRatio>
         <CardContent orientation="horizontal">
-          <AudioPlayer
-            src={`assets/audios/${decade}${type}.mp3`} 
-            className="custom-audio-player"
-            style={{
-              width: 'auto',
-              zoom: 0.7,
-            }}
-          ></AudioPlayer>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <Typography variant="subtitle1" textAlign="center" marginBottom={1} color="grey">
+              {currentSong.map((audio) => audio.title)}
+            </Typography>
+            <Typography component="p" textAlign="center" marginBottom={1} color="grey">
+              {currentSong.map((audio) => audio.singer)}
+            </Typography>
+            <AudioPlayer
+              src={`assets/audios/${decade}${type}.mp3`}
+              className="custom-audio-player"
+              style={{
+                width: "auto",
+                zoom: 0.7,
+              }}
+            ></AudioPlayer>
+          </div>
         </CardContent>
       </Card>
     </div>
@@ -113,10 +130,10 @@ const SideAudio = ({ onClickSelect }: Props) => {
       src={`assets/audios/${decade}${type}.mp3`}
       controls
       style={{
-        position: 'fixed',
-        bottom: '0',
-        width: '20%',
-        height: '20px',
+        position: "fixed",
+        bottom: "0",
+        width: "20%",
+        height: "20px",
       }}
     ></ReactAudioPlayer>
   );
